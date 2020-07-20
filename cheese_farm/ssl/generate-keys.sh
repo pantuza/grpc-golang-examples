ecc7866bbcd12d118702a100cef964c99ad88dff
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-07-20 18:37:40 -0300

    Update x509 certificate to expire in 100 years

--- cheese_farm/ssl/generate-keys.sh
+++ cheese_farm/ssl/generate-keys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -keyout private.key -out cert.pem -days 365 -nodes -subj "/CN=localhost"
+openssl req -x509 -newkey rsa:4096 -keyout private.key -out cert.pem -days 36500 -nodes -subj "/CN=localhost"
